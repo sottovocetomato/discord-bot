62a55b2eced01a88aaf91b5aac8b25d69bc5441b
ai-strings <adrstrings@gmail.com>
2023-12-04 14:16:07 +0300

    -remove unnecessary code

--- src/index.js
+++ src/index.js
@@ -17,29 +17,5 @@ const client = new Client({
   partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
-// client.on("messageCreate", (message) => {
-//   console.log(message);
-//   if (message.author.bot) {
-//     return;
-//   }
-//   message.reply(`Привет, тебе ${message.author.globalName}`);
-// });
-
-//
-// client.on("interactionCreate", (interaction) => {
-//   if (!interaction.isChatInputCommand()) return;
-//   console.log(interaction.commandName);
-//   if (interaction.commandName === "hey") {
-//     interaction.reply(`Привет, тебе, чертила`);
-//   }
-//   if (interaction.commandName === "ping") {
-//     interaction.reply(`Pong`);
-//   }
-//   if (interaction.commandName === "add") {
-//     const num1 = interaction.options.get("first-number")?.value;
-//     const num2 = interaction.options.get("second-number")?.value;
-//     interaction.reply(`Сумма: ${num1 + num2}`);
-//   }
-// });
 eventHandler(client);
 client.login(process.env.TOKEN);
