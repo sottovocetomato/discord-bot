71eafe283e3d22bdf397101cba1b7e441923104d
ai-strings <adrstrings@gmail.com>
2023-12-06 11:35:28 +0300

    !wip fix dotenv

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require("path");
-require("dotenv").config();
+require("dotenv").config({ path: "../../.env" });
 const eventHandler = require("./src/handlers/eventHandler");
 
 const { Client, IntentsBitField, Partials } = require("discord.js");
