c44bf5dbca03a3a7360071f2a903fa70fe717d92
ai-strings <adrstrings@gmail.com>
2023-12-06 11:57:57 +0300

    !wip fix dotenv

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const path = require("path");
-require("dotenv").config({ path: "./.env" });
+require("dotenv").config({ path: path.join(__dirname, "./", "env") });
 const eventHandler = require("./src/handlers/eventHandler");
 
 const { Client, IntentsBitField, Partials } = require("discord.js");
-console.log(__dirname, "__dirname");
+console.log(path.join(__dirname, "./", "env"), "__dirname");
 console.log(process.env.TOKEN, "TOKEN");
 const client = new Client({
   intents: [
