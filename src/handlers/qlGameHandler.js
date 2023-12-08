9c5a2ac4541a49ba7d0d584b662a2b6cf29fda73
ai-strings <adrstrings@gmail.com>
2023-12-08 11:43:00 +0300

    !change data

--- src/handlers/qlGameHandler.js
+++ src/handlers/qlGameHandler.js
@@ -86,9 +86,9 @@ const actions = {
     }
 
     //TODO: убрать блок при переходе на бд
-    const currentWinner = interaction.guild.roles.cache
-      .get(qlWinnerRoleId)
-      .members.map((m) => m.user.id);
+    const currentWinner = interaction?.guild?.roles?.cache
+      ?.get(qlWinnerRoleId)
+      ?.members.map((m) => m.user.id);
     if (currentWinner.length) {
       gameData.currentWinnerId = currentWinner[0];
     }
