14095c92144e1758be44543b636cce63e05776ff
ai-strings <adrstrings@gmail.com>
2023-12-20 15:13:38 +0300

    !remove role models

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ db.sequelize
     console.error("Unable to connect to the database: ", error);
   });
 
-db.sequelize.sync({ force: true }).then(async () => {
+db.sequelize.sync().then(async () => {
   console.log("Synced db.");
 });
 // console.log(result.parsed);
